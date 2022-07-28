d45d825a8da622eccf1c6a0a310b978211b3b170
thoma138 <thomaselliott@live.com>
2022-07-28 17:23:09 +1000

    commit tree 0d012492bda4c7a67f32efb9c5f4904d1ec10b96

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,26 @@
 def unsafe?(speed)
+    if speed < 40
+    true
+    elsif speed > 60
+    true
+    else speed >= 40 && speed <= 60
+    false
+    end
+    
 
 end
 
 
 
 def not_safe?(speed)
-	
+   if speed > 60
+    true
+    
+   elsif speed < 40  
+    true
+   else speed ? speed >= 40 : speed <= 60
+    false
+   end	
 end
 	
 
